ca3a2024a8a4fe835837c318e591210d8d207d08
Siddiqui Abdul Moid - 58 <siddiquimoid@eng.rizvi.edu.in>
2026-02-11 03:07:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-print("--- Factorial Finder ---")
-
 n = int(input("Enter Number: "))
 
 if n < 0:
-
 print(f"Factorial of {n} is Not Defined")
-
 else:
-
 fact = 1
-
 for i in range(1, n + 1):
-
-fact = fact*i
-
+fact *= i
 print(f"Factorial of {n} is {fact}")
