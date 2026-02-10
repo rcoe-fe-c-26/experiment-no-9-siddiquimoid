c75da76a1f2da10df049fdf3445e0310b455384c
Siddiqui Abdul Moid - 58 <siddiquimoid@eng.rizvi.edu.in>
2026-02-11 03:12:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 n = int(input("Enter Number: "))
 
 if n < 0:
-print(f"Factorial of {n} is Not Defined")
+    print("Factorial of", n, "is not defined")
 else:
-fact = 1
-for i in range(1, n + 1):
-fact *= i
-print(f"Factorial of {n} is {fact}")
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    print("Factorial of", n, "is", fact)
